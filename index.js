3fa24807676654ad1add8d990940833e969f31e5
manuligit <riina.vaisanen@gmail.com>
2018-02-13 22:42:45 +0200

    t4.2

--- index.js
+++ index.js
@@ -3,39 +3,26 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Blog = require('./models/blog')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 require('dotenv').config()
-
-
-app.use(cors())
-app.use(bodyParser.json())
 
 const mongoUrl = process.env.MONGODB_URI
 
 mongoose.connect(mongoUrl)
+  .then( () => {
+    console.log('connected to database')
+  })
+  .catch (error => {
+    console.log(error.name)
+  })
 
 mongoose.Promise = global.Promise
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    }).catch(error => {
-      console.log(error)
-    })
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
