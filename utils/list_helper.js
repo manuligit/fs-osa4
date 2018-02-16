e2ae7b2341d99938555515a6c5c48d1bd512c702
manuligit <riina.vaisanen@gmail.com>
2018-02-16 23:35:48 +0200

    t4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -18,6 +18,45 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0 ? [] : blogs.reduce(reducer, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+  //if no blogs, return empty object
+  if (blogs.length === 0) {
+    return {}
+  }
+  //map authors
+  const authors = blogs.map(x => x.author)
+  //console.log(authors)
+  
+  //count unique items in the author list
+  let unique = []
+  let uniquecounts = []
+
+  authors.forEach(element => {
+   if (unique.includes(element)) {
+     console.log('included')
+     index = unique.indexOf(element)
+     uniquecounts[index] += 1
+   } else {
+     unique.push(element)
+     uniquecounts.push(1)
+   }
+  })
+
+  //console.log(unique)
+  //console.log(uniquecounts)
+
+  const findLargest = (largest, item) => {
+    return (item > largest) ? item : largest;
+  }
+  //find largest count and it's index and return it
+  largest = uniquecounts.reduce(findLargest, uniquecounts[0])
+  const largestindex = uniquecounts.findIndex(item => item === largest )
+
+  const item = {author: unique[largestindex], blogs: uniquecounts[largestindex]}
+  console.log(item)
+  return item
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
